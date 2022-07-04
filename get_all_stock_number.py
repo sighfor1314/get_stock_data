1cd1229b6de2ea9aae2696ab686cc8b96ca83b9c
ChiaLing <chialing@ChiaLing-MacBook-Pro.local>
2022-07-04 21:05:48 +0800

    add comment

--- get_all_stock_number.py
+++ get_all_stock_number.py
@@ -8,22 +8,22 @@ class  GetStockNumber:
     def get_number(self):
         stock_dic = {}
 
-        try:
+        try:  # Check parse url is succeeded
             response = requests.get('https://isin.twse.com.tw/isin/C_public.jsp?strMode=2')
         except requests.exceptions.RequestException as e:  # This is the correct syntax
             raise SystemExit(e)
-        soup = BeautifulSoup(response.text, "html.parser")
-        stock_table = soup.find_all("tr")
-        for i in stock_table:
+        soup = BeautifulSoup(response.text, "html.parser")  # Use BeautifulSoup get html data
+        stock_table = soup.find_all("tr")  # find tag 'tr'
+        for i in stock_table: # find tr -> td for getting all stock number and name
             stock_info= i.find_all("td")
             is_number = re.match(r'^[0-9]', stock_info[0].text[0])
-            if stock_info[0].text == " 上市認購(售)權證  ":
+            if stock_info[0].text == " 上市認購(售)權證  ":  # Stop to tr.text = 上市認購(售)權證
                 break
-            if stock_info[0].text[0] == ' ' or not is_number:
+            if stock_info[0].text[0] == ' ' or not is_number: #before tr.text = 上市認購(售)權證 only find stock_number
                 continue
 
-            stock_list=stock_info[0].text.split()
-            stock_dic[stock_list[0]] = stock_info[4].text
+            stock_list=stock_info[0].text.split() # stock_number stock_name to stock_list[stock_number,stock_name]
+            stock_dic[stock_list[0]] = stock_info[4].text # map  industry_type to dictionary {stock_number: industry_type}
 
         return stock_dic
 
