84ade5476dd8cf4eaa6229e9504581e9d4d6d120
sighfor1314 <sighfor1314@gmail.com>
2022-07-07 00:41:41 +0800

    add lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+from get_all_stock_number import GetStockNumber
+from stock_info import GetStockInfo
+from top3_industry import GetTop3Industry
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    task = GetStockNumber()
+    stock_dic = task.get_number()
+
+    task = GetStockInfo(stock_dic)
+    result_dic,industry = task.get_stock_data()
+    task = GetTop3Industry(result_dic,industry)
+    task.convert_to_pd ()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
